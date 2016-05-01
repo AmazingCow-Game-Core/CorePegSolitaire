5b94af608d69ec005ca4d674cb5cc513d6f61e51
n2omatt <n2o.matt@gmail.com>
2016-05-01 18:29:04 -0300

    - Improve documentation.

--- include/GameCore.h
+++ include/GameCore.h
@@ -56,69 +56,88 @@ class GameCore
 {
     // CTOR/DTOR //
 public:
-    ///@brief Constructs a PegSolitaire Game Core with the 
-    ///board loaded by the LevelLoader.
-    ///@param loader A reference of a Board loader.
-    ///@see ILevelLoader.
+    ///@brief
+    ///     Constructs a PegSolitaire Game Core with the
+    ///      board loaded by the LevelLoader.
+    ///@param loader
+    ///     A reference of a Board loader.
+    ///@note
+    ///     If the loader throws an exception, it will NOT be handled
+    ///     by the GameCore - So is users reponsability to handle them.
+    ///@see GameCore::kUnlimitedMoves, ILevelLoader, FileLevelLoader.
     GameCore(const ILevelLoader &loader);
 
 
     // Public Methods //
 public:
-    ///@brief Make a move, checking first if all the coords are valid,
-    ///if them are of the correct types and if them represents a valid 
-    ///movement. If a valid movement is represented it will update the
-    ///game status and the affected coords and pegs.
-    ///After each call of makeMove the user should call the getStatus
-    ///method to check if game is over.
-    ///@param sourceCoord Any CoreCoord::Coord, but if it isn't 
-    ///representing a peg (PegType::Peg) movement won't be valid.
-    ///@param targetCoord Any CoreCoord::Coord, but if it isn't 
-    ///representing a hole (PegType::Hole) and if isn't in a range of valid movements 
-    ///of the sourceCoord the movement won't be valid.
-    ///@returns true if the movement was performed, false otherwise.
-    ///@see CoreCoord::Coord, PegType, Status, getStatus, getMovesForPeg, isValidMove.
-    bool makeMove(const CoreCoord::Coord &sourceCoord, 
+    ///@brief
+    ///     Make a move, checking first if all the coords are valid,
+    ///     if them are of the correct types and if them represents a valid
+    ///     movement.  \n
+    ///     If a valid movement is represented it will update the
+    ///     game status and the affected coords and pegs. \n
+    ///     After each call of makeMove() the user should call the getStatus()
+    ///     method to check if game is over.
+    ///@param sourceCoord
+    ///     Any CoreCoord::Coord, but if it isn't
+    ///     representing a peg (PegType::Peg) movement won't be valid.
+    ///@param targetCoord
+    //      Any CoreCoord::Coord, but if it isn't representing a
+    ///     hole (PegType::Hole) and if isn't in a range of valid
+    //      movements of the sourceCoord the movement won't be valid.
+    ///@returns
+    ///     true if the movement was performed, false otherwise.
+    ///@see
+    ///     CoreCoord::Coord, PegType, Status,
+    ///     getStatus, getMovesForPeg, isValidMove.
+    bool makeMove(const CoreCoord::Coord &sourceCoord,
                   const CoreCoord::Coord &targetCoord);
 
-    
+
     ///@brief Gets the current board representation.
     ///@returns The current board representation.
     const Board& getBoard() const;
 
-    ///@brief Get the type of the Peg at Coord. 
-    ///If coord isn't valid returns Peg::Invalid.
-    ///@param CoreCoord::Coord the coord of queried peg.
-    ///@returns The type of peg. 
+    ///@brief
+    ///     Get the type of the Peg at Coord.
+    ///     If coord isn't valid returns Peg::Invalid.
+    ///@param coord
+    ///     The coord of queried peg.
+    ///@returns The type of peg.
     ///@see PegType, CoreCoord::Coord.
     PegType getPegAt(const CoreCoord::Coord &coord) const;
 
 
-    ///@brief Gets the current status of the game.
-    ///Should be called each time that makeMove is called.
+    ///@brief
+    ///     Gets the current status of the game.
+    ///     Should be called each time that makeMove is called.
     ///@returns The current status of game.
-    ///@see Status, makeMove.
+    ///@see Status, makeMove().
     Status getStatus() const;
 
     ///@brief Gets how many Pegs (PegType::Peg) is remaining.
     ///@returns The pegs count.
     int getPegsCount() const;
 
-    ///@brief Gets how many moves player had performed. 
+    ///@brief Gets how many moves player had performed.
     ///@returns How many moves player did.
     int getMovesCount() const;
 
 
-    ///@brief Gets all the target CoreCoord::Coords that this peg
-    ///can be placed. This is useful if you want to 
-    ///highlight the possible destination of this peg.
-    ///If the CoreCoord::Coord doesn't represents a PegType::Peg
-    ///an empty vector will be returned.
-    ///@param The CoreCoord::Coord of Peg that will be queried.
-    ///@returns All target coords that the peg at CoreCoord::Coord
-    ///can be placed, or an empty vector if CoreCoord::Coord doesn't 
-    ///represents a peg (PegType::Peg).
-    ///@see PegType, isValidCoord, makeMove
+    ///@brief
+    ///     Gets all the target CoreCoord::Coords that this peg
+    ///     can be placed. This is useful if you want to
+    ///     highlight the possible destination of this peg. \n
+    ///     If the CoreCoord::Coord doesn't represents a PegType::Peg
+    ///     an empty vector will be returned.
+    ///@param coord
+    //      The coord of Peg that will be queried.
+    ///@returns
+    ///     All target coords that the peg at CoreCoord::Coord
+    ///     can be placed, or an empty vector if CoreCoord::Coord doesn't
+    ///     represents a peg (PegType::Peg).
+    ///@see
+    ///     PegType, isValidCoord, makeMove
     CoreCoord::Coord::Vec getMovesForPeg(const CoreCoord::Coord &coord) const;
 
     ///@brief Get all CoreCoord::Coords thats represents a Peg (PegType::Peg).
@@ -138,20 +157,25 @@ public:
 
 
     ///@brief Check if a coord is valid i.e. is inside of Board bounds.
-    ///@param coord A CoreCoord::Coord that will be queried.
-    ///@returns True if coord is inside of Board bounds, false otherwise.
+    ///@param
+    ///     coord A CoreCoord::Coord that will be queried.
+    ///@returns
+    ///     True if coord is inside of Board bounds, false otherwise.
     ///@see Coord, Board.
     bool isValidCoord(const CoreCoord::Coord &coord) const;
 
     ///@brief Check if the two coords can represent a valid move.
-    ///@param sourceCoord Any coord, but if it isn't valid or isn't representing
-    ///a Peg (PegType::Peg) the movement is not valid.
-    ///@param targetCoord Any coord, but if it isn't valid or isn't representing
-    ///a Hole (PegType::Hole) the movement is not valid. Furthermore the 
-    ///targetCoord must be in range of the sourceCoord.
-    ///@returns True is is valid movement, false otherwise.
-    ///@see Coord, PegType, getMovesForPeg, isValidCoord.
-    bool isValidMove(const CoreCoord::Coord &sourceCoord, 
+    ///@param sourceCoord
+    ///     Any coord, but if it isn't valid or isn't representing
+    ///     a Peg (PegType::Peg) the movement is not valid.
+    ///@param targetCoord
+    ///     Any coord, but if it isn't valid or isn't representing
+    ///     a Hole (PegType::Hole) the movement is not valid. Furthermore the
+    ///     targetCoord must be in range of the sourceCoord.
+    ///@returns
+    ///     True is is valid movement, false otherwise.
+    ///@see Coord, PegType, getMovesForPeg(), isValidCoord().
+    bool isValidMove(const CoreCoord::Coord &sourceCoord,
                      const CoreCoord::Coord &targetCoord) const;
 
 
